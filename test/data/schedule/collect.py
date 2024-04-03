64b2c9a06308b60b03a851a3f58a423a0472d49d
Josh Harkness <joshua.harkness32@gmail.com>
2024-04-02 21:35:32 -0400

    Added test for away_days_since_previous games too

--- test/data/schedule/collect.py
+++ test/data/schedule/collect.py
@@ -13,12 +13,16 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(week_one['home_days_since_previous_game'].unique(), [240])
 
     def test_days_since_previous_game(self):
-        # DET opened on Thursday and played on Sunday in week 2
+        # DET opened on Thursday as the away team and played home on Sunday in week 2
         week_two_det = self.schedule_data[
             (self.schedule_data['week'] == 2) & (self.schedule_data['home_team'] == 'DET')
         ]
-        print(week_two_det)
         self.assertEqual(week_two_det['home_days_since_previous_game'].values[0], 10)
+        # KC played DET as the home team on Thursday in week 1 and played away on Sunday in week 2
+        week_two_kc = self.schedule_data[
+            (self.schedule_data['week'] == 2) & (self.schedule_data['away_team'] == 'KC')
+        ]
+        self.assertEqual(week_two_kc['away_days_since_previous_game'].values[0], 10)
         # ARI had a bye week in week 14
         week_fourteen_ari = self.schedule_data[
             (self.schedule_data['week'] == 15) & (self.schedule_data['home_team'] == 'ARI')
