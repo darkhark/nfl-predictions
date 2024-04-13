f78085ddb4c121abb5787c44a790e2e2efa9d121
Josh Harkness <joshua.harkness32@gmail.com>
2024-04-13 10:26:31 -0400

    Merged the weekly data with the schedule data to create home and away team statistics in a single row.

--- src/data/collect_all.py
+++ src/data/collect_all.py
@@ -40,9 +40,34 @@ def get_schedule_and_weekly_data(years):
     weekly_data = weekly_collect.get_weekly_data(years).reset_index(drop=True)
     schedule_data = schedule_collect.get_schedule_data(years).reset_index(drop=True)
 
+    home_team_is_team = _get_home_or_away_team_in_weekly_data(schedule_data, weekly_data, is_home=True)
+    away_team_is_team = _get_home_or_away_team_in_weekly_data(schedule_data, weekly_data, is_home=False)
+    combined_data = pd.merge(
+        home_team_is_team,
+        away_team_is_team,
+        on=['season', 'week', 'home_team', 'away_team'],
+        suffixes=('', '_y')
+    )
+    # drop the columns with _y suffix
+    combined_data = combined_data[
+        [col for col in combined_data.columns if not col.endswith('_y')]
+    ]
+
+    #
+
+    return combined_data
+
+
+def _get_home_or_away_team_in_weekly_data(schedule_data, weekly_data, is_home=True):
+    if is_home:
+        team_type = 'home'
+        opp_team_type = 'away'
+    else:
+        team_type = 'away'
+        opp_team_type = 'home'
     home_team_is_team = schedule_data.merge(
         weekly_data,
-        left_on=['home_team', 'season', 'week'],
+        left_on=[f'{team_type}_team', 'season', 'week'],
         right_on=['team', 'season', 'week'],
         how='inner'
     )
@@ -50,51 +75,14 @@ def get_schedule_and_weekly_data(years):
     home_team_is_team.drop(columns=['team', 'opp_team'], inplace=True)
     # in every column that contains 'opp', replace 'opp' with 'away'
     home_team_is_team = home_team_is_team.rename(
-        columns=lambda x: x.replace('opp', 'away') if 'opp' in x else x
+        columns=lambda x: x.replace('opp', opp_team_type) if 'opp' in x else x
     )
     # in every column that contains 'off', replace 'off' with 'home'
     home_team_is_team = home_team_is_team.rename(
-        columns=lambda x: x.replace('off', 'off_home') if 'off' in x else x
+        columns=lambda x: x.replace('off', f'off_{team_type}') if 'off' in x else x
     )
     # in every column that starts with 'team_', replace 'team_' with 'home_'
-    home_team_is_team = home_team_is_team.rename(
-        columns=lambda x: x.replace('team_', 'home_') if 'team_' in x else x
+    return home_team_is_team.rename(
+        columns=lambda x: x.replace('team_', f'{team_type}_') if 'team_' in x else x
     )
 
-    away_team_is_team = schedule_data.merge(
-        weekly_data,
-        left_on=['away_team', 'season', 'week'],
-        right_on=['team', 'season', 'week'],
-        how='inner'
-    )
-
-    # replace the 'team' column with 'away_team'
-    away_team_is_team.drop(columns=['team', 'opp_team'], inplace=True)
-    # in every column that contains 'opp', replace 'opp' with 'home'
-    away_team_is_team = away_team_is_team.rename(
-        columns=lambda x: x.replace('opp', 'home') if 'opp' in x else x
-    )
-    # in every column that contains 'off', replace 'off' with 'away'
-    away_team_is_team = away_team_is_team.rename(
-        columns=lambda x: x.replace('off', 'off_away') if 'off' in x else x
-    )
-    # in every column that starts with 'team_', replace 'team_' with 'away_'
-    away_team_is_team = away_team_is_team.rename(
-        columns=lambda x: x.replace('team_', 'away_') if 'team_' in x else x
-    )
-    # merge the dataframes, but do not create any new columns where the names are the same
-    combined_data = pd.merge(
-        home_team_is_team,
-        away_team_is_team,
-        on=['season', 'week', 'home_team', 'away_team'],
-        suffixes=('', '_y')
-    )
-
-    # drop the columns with _y suffix
-    combined_data = combined_data[
-        [col for col in combined_data.columns if not col.endswith('_y')]
-    ]
-
-
-
-    return combined_data
